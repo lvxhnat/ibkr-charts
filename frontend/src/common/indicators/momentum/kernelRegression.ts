0d180643d2f6759c1f27b712cc515206307ba9b0
Loh Yi Kuang <yikuang@synthesis.partners>
2024-07-17 12:56:05 +0800

    init: kernel regression

--- frontend/src/common/indicators/momentum/kernelRegression.ts
+++ frontend/src/common/indicators/momentum/kernelRegression.ts
@@ -5,38 +5,39 @@ export const defaultKRParams: KernelRegressionParams = {
   kernelType: "gaussian",
 };
 
+/**
+ * @param x_i the observed data point, we create one distribution per data pt, so this should be static
+ * @param x the gaussian x
+ * @param h bandwidth
+ * @returns 
+ */
 const gaussian = (x_i: number, x: number, h: number) => {
   const A = 1 / (h * Math.sqrt(2 * Math.PI));
-  const B = -0.5 * ((x - x_i) / h) ** 2;
+  const B = -0.5 * (((x - x_i) / h) ** 2);
   const K = A * Math.exp(B);
   return K;
-};
-
-const kernelY = (
-  x_i: number,
-  data: number[],
-  kernel: (x_i: number, x: number, h: number) => number,
-  h: number
-) => {
-  return data.reduce((acc, point) => {
-    const w_i = kernel(x_i, point, h);
-    return acc + w_i * point;
-  }, 0);
 };
 
 const kernels = {
   gaussian: gaussian,
 };
 
+const calculateKernelPoint = (
+  x_i: number,
+  data: number[],
+  kernel: Function,
+  h: number
+) => {
+  const density: number[] = data.map((_, i) => kernel(x_i, i + 1, h))
+  const sumDensity = density.reduce((a, b) => a + b, 0)
+  return density.map((d, i) => data[i] * (d / sumDensity)).reduce((a, b) => a + b, 0)
+}
+
 export function calculateKernelRegression(
   data: number[],
   params: KernelRegressionParams
 ): number[][] {
-  const h = params.bandwidth ?? defaultKRParams.bandwidth;
-  const kernel = params.kernelType ?? defaultKRParams.kernelType;
-  const result = data.map((pt) =>
-    kernelY(pt, data, kernels[kernel as keyof typeof kernels], h!)
-  );
-
-  return [result];
+  const h: number = params.bandwidth ?? defaultKRParams.bandwidth!;
+  const kernel = kernels[params.kernelType ?? defaultKRParams.kernelType as keyof typeof kernels];
+  return [data.map((d, i) => calculateKernelPoint(i, data, kernel, h))];  
 }
